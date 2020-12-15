3a10770cad140ed035a7b6ef35343b194e80199d
dastan08 <toaditya1999@gmail.com>
2020-12-16 03:51:29 +0530

    project, member page updated

--- js/main.js
+++ js/main.js
@@ -26,3 +26,28 @@ document.querySelector("#signup").addEventListener("click", () => {
     card.classList.add("rotate");
 });
 
+
+//project-function
+(function () {
+
+    let makeClickable = (element, linkContext) => {
+        element.addEventListener("click", event => {
+            var link = linkContext.querySelector("a");
+            if (link && link.getAttribute("href")) {
+
+                window.open(
+                    link.getAttribute("href"),
+                    link.getAttribute("target")
+                );
+
+                event.preventDefault();
+            }
+        });
+    };
+
+    document.querySelectorAll(".box").forEach(box => {
+        box.querySelectorAll(".image,h3").forEach(element =>
+            makeClickable(element, box));
+    });
+
+})();
